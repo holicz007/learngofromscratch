1240d5da8d28f240497980a6aa840f8ecf5489cd
twin-dev <i3boy_pim@hotmail.com>
2018-05-16 17:14:22 +0700

    Go Approach , Custom Type , Receiver Functions

--- main.go
+++ main.go
@@ -4,45 +4,12 @@ import (
 	"fmt"
 )
 
-/*
-	arrays : กำหนดขนาดชัดเจน
-
-	slice : ไม่่จำเป็นต้องระบุขนาด (List) และ ไทป์ของสิ่งที่เก็บต้องเหมือนกันทั้งหมด
-*/
-
 func main() {
 
-	/*
-		การประกาศ slice
-		[] ประเภทตัวแปร{values,values}
-	*/
+	cards := deck{"Ace of Diamonds", "Five of Diamonds"}
 
-	cards := []string{newCard(), newCard(), newCard()}
+	fmt.Println(cards)
 
-	/*
-		การเพิ่มค่าให้กับ slice
-		ตัวแปรประเภท slice = append(ตัวแปรประเภท slice นั้น ๆ , values)
+	cards.print()
 
-		** Note : เหมือนกับว่ามันไม่ได้แก้ไข slice ตัวก่อนแต่มันสร้างใหม่แล้วเอาไปทับของเดิม
-	*/
-
-	cards = append(cards, "Six of Spades")
-
-	/*
-		การนำค่าใน slice มาใช้งานด้วย for loops
-
-		for อินเด็กซ์ , ชื่อตัวแปร (ค่าของช่องนั้น ๆ จะอยู่ในตัวแปรนี้) := range ตัวแปร slice ที่เราจะวน {
-			
-		}
-	*/
-
-	for i, card := range cards {
-		fmt.Println(i, card)
-
-	}
-
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }

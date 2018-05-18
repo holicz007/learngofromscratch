cbf2eee526270887f3c730effb480229c5b9ba56
twin-dev <i3boy_pim@hotmail.com>
2018-05-18 11:22:51 +0700

    finish assignments

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,38 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Excepted deck lenght of 20, but got %d ", len(d))
+		t.Errorf("Excepted deck lenght of 20, but got %v ", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades , but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Club" {
+		t.Errorf("Expected last card is Four of Clubs , but got %v", d[len(d)-1])
+	}
+
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected card must be 16 , but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
